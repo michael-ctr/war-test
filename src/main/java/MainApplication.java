e6de4304afab7f8b9c5e0c61188f25d620a8a8bf
michael-ctr <freezingdeep@yahoo.com>
2017-11-29 14:36:26 +1100

    initial project

---
+++ src/main/java/MainApplication.java
@@ -0,0 +1,8 @@
+/**
+ * Created by MC on 29/11/17.
+ */
+public class MainApplication {
+    public static void main(String[] args) {
+        System.out.println("running stuff");
+    }
+}
---
+++ src/test/java/FakeTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+/**
+ * Created by MC on 29/11/17.
+ */
+public class FakeTest {
+    @Test
+    public void testStuff() {
+        System.out.println("test stuff");
+    }
+
+}
